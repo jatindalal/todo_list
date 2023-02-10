dba71db5af649ecfe8d6c904e250f23afba95e6c
Jatin Dalal <jd.jatindalal@gmail.com>
2023-02-10 15:05:29 +0530

    hack dockerfile, update req and load env properly

--- dockerfile
+++ dockerfile
@@ -5,7 +5,7 @@ COPY . /app
 RUN chown -R www-data:www-data /app
 
 RUN pip install -r requirements.txt --no-cache-dir
-RUN export DJANGO_SECRET=$(python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
+RUN echo DJANGO_SECRET=$(python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())') >> .env
 
 EXPOSE 80
 CMD [ "gunicorn", "todo_list.wsgi", "--user", "www-data", "--bind", "0.0.0.0:80", "--workers", "3" ]
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,14 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=''
-if "DJANGO_SECRET" in os.environ:
-    SECRET_KEY = os.environ.get("DJANGO_SECRET")
-else:
-    SECRET_KEY = 'django-insecure-p)#yd)to*uz^u!s!+ph_0!x@r^uxvde5pj5$va*6w-7=4qhfv4'
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
